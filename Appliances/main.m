e3ebbd8a56312dec3a3ac4c1c40b8e849a1fabb4
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-18 12:51:09 -0700

    Working but wierd symbols around my first owner name

--- Appliances/main.m
+++ Appliances/main.m
@@ -8,17 +8,23 @@
 
 #import <Foundation/Foundation.h>
 #import "BNRAppliance.h"
+#import "BNROwnedAppliance.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         BNRAppliance *a = [[BNRAppliance alloc] init];
+        BNROwnedAppliance *b = [[BNROwnedAppliance alloc] initWithProductName:@"Dryer" firstOwnerName:@"Jamie"];
+    
 
-        //BNRAppliance *a = [[BNRAppliance alloc] init];
+     
         NSLog(@"The product name is %@", [a description]);
         a.productName = @"Washing Machine";
         a.voltage = 240;
+       
+        NSLog(@"The product name is now %@ and the owners name is %@", b.productName, b.ownerNames);
         NSLog(@"The product name is now %@ and the voltage is %i", a.productName, a.voltage);
+        
     }
     return 0;
 }
